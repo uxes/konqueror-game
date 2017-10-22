12c8cdd5bc20196419d95376b12592b146206ad2
Adam Havelka <linuxes@mineland.eu>
2017-10-22 18:13:56 +0200

    ws connection in thread

--- app/src/main/java/cz/uxes/konqueror_game/UsersListActivity.java
+++ app/src/main/java/cz/uxes/konqueror_game/UsersListActivity.java
@@ -2,14 +2,16 @@ package cz.uxes.konqueror_game;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import cz.uxes.konqueror_game.network.WsConnection;
-import cz.uxes.konqueror_game.network.WsConnectionSingleton;
 
 public class UsersListActivity extends AppCompatActivity {
+
+    private WsConnection ws;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +23,30 @@ public class UsersListActivity extends AppCompatActivity {
         String[] fruits = new String[] { "Uxes", "Cosik", "Jakysik hrač", "Jakysik jiny hrač" };
 
         //todo: should not use singleton, thread seems to be threaten
-        WsConnectionSingleton ws = WsConnectionSingleton.INSTANCE;
 
 
-        ws.connection.start();
-        //ws.connection.run("10.0.0.25");
+        ws =  new WsConnection();
+        ws.execute("10.0.0.25");
 
-        //ws.webSocket.sendText("text");
-
-        //ws.fetchUsers();
 
         ListAdapter li = new ArrayAdapter<String>(this, R.layout.user_row, R.id.userName, fruits);
         listView.setAdapter(li);
+
+    }
+
+    public void loadPlayers(View view){
+
+        ws.fetchUsers();
+
+
+        ListView listView = (ListView) findViewById(R.id.userList);
+
+        String[] fruits = new String[] { "Uxes" };
+
+
+        ListAdapter li = new ArrayAdapter<String>(this, R.layout.user_row, R.id.userName, fruits);
+        listView.setAdapter(li);
+
 
     }
 
--- app/src/main/java/cz/uxes/konqueror_game/network/WsConnection.java
+++ app/src/main/java/cz/uxes/konqueror_game/network/WsConnection.java
@@ -1,12 +1,19 @@
 package cz.uxes.konqueror_game.network;
 
+import android.os.AsyncTask;
 import android.util.Log;
+
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.neovisionaries.ws.client.WebSocket;
 import com.neovisionaries.ws.client.WebSocketAdapter;
 import com.neovisionaries.ws.client.WebSocketFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,23 +21,69 @@ import java.util.List;
  * Created by uxes on 18.10.17.
  */
 
-public class WsConnection extends Thread {
+public class WsConnection extends AsyncTask<String, Void, String> {
 
     public WebSocket webSocket;
 
-    public void run(String server){
+    public List<User> fetchUsers() {
+
+        try{
+
+            webSocket.sendText("{\"onlinePlayers\": true}");
+
+
+
+
+            return new ArrayList<>();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return new ArrayList<>();
+
+        }
+
+    }
+
+    @Override
+    protected String doInBackground(String... server) {
 
         try{
 
             webSocket = new WebSocketFactory()
                     .setConnectionTimeout(5000)
-                    .createSocket("ws://" + server + ":8124")
+                    .createSocket("ws://" + server[0] + ":8124")
                     .addProtocol("connection")
                     .addListener(new WebSocketAdapter() {
                         // A text message arrived from the server.
-                        public void onTextMessage(WebSocket websocket, String message) {
+                        public void onTextMessage(WebSocket websocket, String message) throws JSONException, IOException {
                             System.out.println(message);
                             Log.d("cosiks", message);
+
+
+
+                            JSONObject jObject = new JSONObject(message);
+                            String aJsonString = jObject.getString("players");
+                            Log.d("co tam je?", aJsonString);
+
+                            ObjectMapper objectMapper = new ObjectMapper();
+                            objectMapper.configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, true);
+
+                            TypeReference<List<User>> mapType = new TypeReference<List<User>>() {};
+
+                            //todo: nějak se mu nechce parsovat..
+                            List<User> returner = objectMapper.readValue(aJsonString, mapType);
+
+                            for (User u : returner) {
+                                Log.d("cosik", u.getNick());
+
+                            }
+
+
+
+                            Log.d("bloody", objectMapper.writeValueAsString(returner));
+
+
+                            Log.d("cosiksy", message);
                         }
                     })
                     .connect();
@@ -39,31 +92,6 @@ public class WsConnection extends Thread {
         catch (Exception e){
             e.printStackTrace();
         }
-
-
-    }
-
-    public List<User> fetchUsers() {
-        webSocket.sendText("{\"users\": true}");
-
-        List<User> returner = new ArrayList<>();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, true);
-
-
-
-        webSocket.addListener(new WebSocketAdapter(){
-            public void onTextMessage(WebSocket ws, String msg) throws Exception{
-
-                Log.d("ws cosik", msg);
-            }
-
-        });
-
-
-
-
-        return returner;
+        return null;
     }
 }
